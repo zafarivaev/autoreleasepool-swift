21e2332787f645c690baf50f850812b3c77489a9
zafarivaev <z.ivaev@mail.ru>
2022-01-16 22:30:58 +0500

    Add files

--- Autoreleasepool/ViewController.swift
+++ Autoreleasepool/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let queue = DispatchQueue(label: "...")
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        overrideUserInterfaceStyle = .light
+        
+        loadImages()
     }
 
-
+    func loadImages() {
+        let url = URL(string: "https://picsum.photos/200")!
+        
+        for _ in 0...500 {
+            queue.async { [unowned self] in
+                autoreleasepool {
+                    guard let data = try? Data(contentsOf: url),
+                          let image = UIImage(data: data) else {
+                              return
+                          }
+                    
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                    }
+                }
+            }
+        }
+    }
+    
 }
 
