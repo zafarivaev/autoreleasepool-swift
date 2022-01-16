3bbd6203afdb7af0d0168bd2a60d0a6f370090dc
zafarivaev <z.ivaev@mail.ru>
2022-01-16 14:31:49 +0500

    Initial Commit

---
+++ Autoreleasepool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autoreleasepool
+//
+//  Created by Зафар Иваев on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
